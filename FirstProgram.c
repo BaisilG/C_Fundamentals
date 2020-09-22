9dfe62f535c4ad5a812dbd99dd0df49c5cba2c53
baisilg <pgbaisil@gmail.com>
2020-09-22 18:15:02 +0530

    updates

--- FirstProgram.c
+++ FirstProgram.c
@@ -1,11 +1,9 @@
    #include<stdio.h>
+   #include<iostream.h>
     #include<conio.h>
     void main()
     {
-    int a=10, b=20;
-    int sum=0;
-    clrscr();  // use clrscr() after variable declaration
-    sum=a+b;
-    printf("Sum: %d",s);
+    clrscr();
+    printf("Baisil");
     getch();
     }
