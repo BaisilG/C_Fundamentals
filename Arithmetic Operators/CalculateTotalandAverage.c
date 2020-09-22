c83ebac75a6ec6082f6e1028de15bbf405d69750
baisilg <pgbaisil@gmail.com>
2020-09-22 18:52:32 +0530

    updates_ Average

--- Arithmetic Operators/CalculateTotalandAverage.c
+++ Arithmetic Operators/CalculateTotalandAverage.c
@@ -1,5 +1,31 @@
-#include<stdio.h>
-#include<conio.h>
+// #include<stdio.h>
+// #include<conio.h>
+
+// main()
+// {
+//     int rolNumb, m1, m2, m3, total;
+//     float average;
+//     printf("Enter your rollNumber");
+//     scanf("%d", &rolNumb);
+//     printf("Enter your Physics Mark");
+//     scanf("%d", &m1);
+//     printf("Enter your Chemistry mark");
+//     scanf("%d", &m2);
+//     printf("Enter your Mathematics mark");
+//     scanf("%d", &m3);
+
+//     total = m1+m2+m3;
+//     average total/3.0;
+
+//     printf ("Average is %f", average);
+
+
+
+// }
+
+
+
+#include <stdio.h>
 
 main()
 {
@@ -15,10 +41,9 @@ main()
     scanf("%d", &m3);
 
     total = m1+m2+m3;
-    average total/3.0;
+    average = total/3.0;
 
     printf ("Average is %f", average);
     
-
 
 }
