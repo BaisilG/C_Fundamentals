a1600fc133f429d581c2f96706504683fca1eefb
baisilg <pgbaisil@gmail.com>
2020-09-22 19:36:13 +0530

    update

--- Arithmetic Operators/Swap.c
+++ Arithmetic Operators/Swap.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-main()
-{
-    int a,b,temp;
+// main()
+// {
+//     int a,b,temp;
+
+//     printf("Enter your number a");
+//     scanf("%d",&a);
+//     printf("Your Number a before swap is %d", a);
+//     printf("Enter your number b");
+//     scanf("%d", &b);
+//     printf("your Number b before swap is %d", b);
     
-}
+
+
+// }
