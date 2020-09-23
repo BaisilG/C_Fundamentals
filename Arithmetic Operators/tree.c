dfd6f2b7153af77f1c07bd1e676e24d749a5eefb
baisilg <pgbaisil@gmail.com>
2020-09-23 22:42:40 +0530

    tree.c

---
+++ Arithmetic Operators/tree.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+    printf("////\\");
+      printf("////\\");
+        printf("////\\");
+          printf("////\\");
+            printf("////\\");
+              printf("////\\");
+                printf("////\\");
+                
+
+}
