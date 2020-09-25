cfd3f87df778862c2bb96ba85e389443626c1ef0
baisilg <pgbaisil@gmail.com>
2020-09-25 22:24:54 +0530

    C Program to Multiply Two Floating-Point Numbers

--- Arithmetic Operators/Examples/C Program to Multiply Two Floating-Point Numbers.c
+++ Arithmetic Operators/Examples/C Program to Multiply Two Floating-Point Numbers.c
@@ -7,11 +7,11 @@ int main() {
     printf("Enter two numbers: ");
     scanf("%lf %lf", &a, &b);  
  
-    // Calculating product
-    product = a * b;
+//     // Calculating product
+//     product = a * b;
 
-    // Result up to 2 decimal point is displayed using %.2lf
-    printf("Product = %.2lf", product);
+//     // Result up to 2 decimal point is displayed using %.2lf
+//     printf("Product = %.2lf", product);
     
-    return 0;
-}
+//     return 0;
+// }
