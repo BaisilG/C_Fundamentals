bd7d62042deeb67cd465beb28595a3d847c4ac5a
baisilg <pgbaisil@gmail.com>
2020-09-27 18:51:41 +0530

    array basic

---
+++ Arithmetic Operators/Examples/array_basic.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>\
+
+int main()
+{
+    int marks[] = {10,20,30,40,50,60,70};
+    int total;
+    for(int i=0; i<7; i++){
+    
+         total = total + marks[i] ;
+
+    }
+
+printf("total is %d", total);
+ return 0;   
+}
